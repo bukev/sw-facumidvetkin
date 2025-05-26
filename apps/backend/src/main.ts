1ddf3a0fa00b6f514d453503dd259ce14a38d734
bukev <bukev@pop-os.localdomain>
2025-05-26 08:47:13 -0300

    enable cors for deployed frontend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000'
+    origin: [
+      'http://localhost:3000',
+      'https://sw-facumidvetkin-frontend.vercel.app'
+    ]
   });
 
   await app.listen(4000, '0.0.0.0');
